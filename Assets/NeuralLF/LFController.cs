2680353d983cf4fbe4a591b313a6a1cb2b697fd7
Suleyman Aslan <suleymanaslan15@gmail.com>
2022-03-23 17:42:18 -0400

    camera array

---
+++ Assets/NeuralLF/LFController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+using static UnityEngine.Mathf;
+
+public class LFController : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Transform gridPrefab;
+
+    [SerializeField, Range(4, 256)]
+    int resolution;
+
+    [SerializeField, Range(0.1f, 1f)]
+    float spherePercentage;
+
+    Transform[] points;
+
+    bool paused;
+
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            transform.position = target.position + target.forward;
+            transform.LookAt(target);
+            if (paused)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
+            paused = !paused;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CreateGrid();
+        }
+    }
+
+    void CreateGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        points = new Transform[resolution * resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(gridPrefab);
+        }
+        float step = 2f / (resolution - 1);
+        float u = -1f;
+        GameObject uChild = new GameObject("(-1)");
+        uChild.transform.SetParent(transform, false);
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                u = z * step - 1f;
+                uChild = new GameObject("(" + u + ")");
+                uChild.transform.SetParent(transform, false);
+            }
+            float v = (x * step - 1f);
+            points[i].SetParent(uChild.transform, false);
+            points[i].localPosition = Sphere(-u, -v);
+            points[i].gameObject.name = "(" + u + "," + v + ")";
+            points[i].transform.LookAt(target);
+        }
+        transform.position = target.position;
+    }
+
+    public Vector3 Sphere(float u, float v)
+    {
+        float r = 1f + Cos(0.5f * PI * spherePercentage * v);
+        Vector3 p;
+        p.x = r * Sin(PI + PI * spherePercentage * u);
+        p.y = Sin(PI * spherePercentage * 0.5f * v);
+        p.z = r * Cos(PI + PI * spherePercentage * u);
+        return p;
+    }
+}
