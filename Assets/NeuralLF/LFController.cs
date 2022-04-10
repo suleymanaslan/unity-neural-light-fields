8e154033706c3da6d88b97fe8ffc92708cc12184
Suleyman Aslan <suleymanaslan15@gmail.com>
2022-04-10 17:36:10 -0400

    Update LFController.cs

--- Assets/NeuralLF/LFController.cs
+++ Assets/NeuralLF/LFController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 using static UnityEngine.Mathf;
 
@@ -16,15 +17,22 @@ public class LFController : MonoBehaviour
     [SerializeField, Range(15, 165)]
     int degrees;
 
+    [SerializeField, Range(1.0f, 2.5f)]
+    float scale;
+
     float SpherePercentage => degrees / 360f;
 
     bool paused;
+
+    bool animate;
+
+    Transform animatedPoint;
 
     Transform[] points;
 
     private void Awake()
     {
-
+        transform.localScale = scale * Vector3.one;
     }
 
     void Start()
@@ -34,7 +42,8 @@ public class LFController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        
+        if (Keyboard.current.pKey.wasReleasedThisFrame)
         {
             transform.position = target.position + target.forward;
             transform.LookAt(target);
@@ -48,19 +57,29 @@ public class LFController : MonoBehaviour
             }
             paused = !paused;
         }
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Keyboard.current.iKey.wasReleasedThisFrame)
         {
             IterateGrid();
         }
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Keyboard.current.gKey.wasReleasedThisFrame)
         {
             CreateGrid();
+        }
+        if (Keyboard.current.aKey.wasReleasedThisFrame)
+        {
+            animate = true;
+            animatedPoint = Instantiate(gridPrefab);
+            animatedPoint.SetParent(transform, false);
+        }
+        if (animate)
+        {
+            AnimateGrid();
         }
     }
 
     private void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Keyboard.current.cKey.wasReleasedThisFrame)
         {
             foreach (Transform child in transform)
             {
@@ -72,13 +91,18 @@ public class LFController : MonoBehaviour
         }
     }
 
+    void AnimateGrid()
+    {
+        animatedPoint.localPosition = TimeSphere();
+        animatedPoint.transform.LookAt(target);
+    }
+
     void IterateGrid()
     {
         foreach (Transform child in transform)
         {
             Destroy(child.gameObject);
         }
-        Transform point = Instantiate(gridPrefab);
         float step = 2f / (resolution - 1);
         float u = -1f;
         GameObject uChild = new GameObject("-1");
@@ -93,12 +117,14 @@ public class LFController : MonoBehaviour
                 uChild = new GameObject("" + u);
                 uChild.transform.SetParent(transform, false);
             }
+            Transform point = Instantiate(gridPrefab);
             float v = (x * step - 1f);
             point.SetParent(uChild.transform, false);
             point.localPosition = Sphere(-u, -v);
             point.gameObject.name = x.ToString("D3") + "-" + z.ToString("D3");
             point.transform.LookAt(target);
             point.GetComponent<CameraCapture>().CamCapture();
+            Destroy(point.gameObject);
         }
         transform.position = target.position;
     }
@@ -135,6 +161,18 @@ public class LFController : MonoBehaviour
             points[i].transform.LookAt(target);
         }
         transform.position = target.position;
+    }
+
+    public Vector3 TimeSphere()
+    {
+        float t = Sin(PI * Time.unscaledTime * 0.25f);
+        float t2 = t * 4;
+        float r = Cos(0.5f * PI * SpherePercentage * t);
+        Vector3 p;
+        p.x = r * Sin(PI + PI * SpherePercentage * t2);
+        p.y = Sin(PI * SpherePercentage * 0.5f * t);
+        p.z = r * Cos(PI + PI * SpherePercentage * t2);
+        return p;
     }
 
     public Vector3 Sphere(float u, float v)
